69719c4e3e816919fe923626f4abd6a586ad8383
mustang85 <sergeant-kir@mail.ru>
2015-01-11 19:56:28 +0300

    right version homewokr

--- app/js/controllers/CardCtrl.js
+++ app/js/controllers/CardCtrl.js
@@ -1,12 +1,51 @@
 app.controller('CardCtrl', ['$scope', 'InboxFactory', function($scope,InboxFactory) {
-	$scope.txt = 'text';
+	// InboxFactory.getMessage()
+	// 	.success(function(jsonData, statusCode) {
+	// 		$scope.uCard = jsonData;
+	// 	});
+	
+	$scope.idUser = location.href.slice((location.href.lastIndexOf('/') + 1)),
+	$scope.uCard = InboxFactory.getCardObj(),
+	$scope.flag = true,
+	userInputs = $('.info-user input'),
+	userLabels = $('.info-user label');
 
-	  InboxFactory.getMessage()
-	  .success(function(jsonData, statusCode) {
-	    $scope.uCard = jsonData;
-	  });
+	function setAttrInput(atr,value) {
+		userInputs.attr(atr,value);
+	}
 
-	$scope.flag = false;
-	$scope.idUser = location.href.slice((location.href.lastIndexOf('/') + 7));
-	console.log($scope.idUser);
+	$scope.editCard = function () {
+		$scope.flag = !$scope.flag;
+		userInputs
+			.removeAttr('disabled');
+		userLabels
+			.addClass('animated pulse');
+	}
+
+	// $scope.setCard = function () {
+	// 	InboxFactory.setCard({'text':'test'});
+	// }
+
+	$scope.removeCard = function (id) {
+		// InboxFactory.removeCard();
+		InboxFactory.removeCard(id);
+	}
+
+	$scope.submitForm = function (json) {
+		InboxFactory.setCard(json);
+	}
+
+	if ($scope.idUser) {
+		// console.log(typeof $scope.idUser == 'number');
+		if ($scope.idUser == 'card') {
+			return;
+		}
+		
+		$scope.flag = false;
+		setAttrInput('disabled','disabled');	
+	}
+		
+	// console.log($scope.uCard);
+	
+	
 }]);
---
+++ app/js/directive/bookPanelElement/bookPanelElement.js
@@ -0,0 +1,135 @@
+app.directive('bookPanelElement', function () {
+        
+  return {
+    restrict: 'EA',
+    scope: true,
+    templateUrl: "js/directive/bookPanelElement/bookPanelElement.html",
+    controller: ('BookPanelCtrl', ['$scope', 'InboxFactory', function($scope,InboxFactory) {
+      $scope.tab = 1,
+      $scope.flagFilter = false,
+      $scope.flagFind = false;
+
+      $scope.filterCard = function () {        
+        return $scope.flagFilter = !$scope.flagFilter;
+      };
+
+      $scope.findCard = function () {        
+        return $scope.flagFind = !$scope.flagFind;
+      };      
+
+      $scope.selectTab = function (setTab) {
+        $scope.tab = setTab;
+      };
+
+      $scope.isSelected = function (checkTab) {
+         return $scope.tab === checkTab;
+      };
+
+      // $scope.searchByCard = function (e) {
+      //    console.log(typeof $scope.findCardUser);
+      //    console.log($scope.listCardsArr);
+      //    var t = 'Granev';
+      //    var tds = $( "td");
+      //    console.log(tds);
+      //    $( "td:contains("+$scope.findCardUser+")" ).css( "text-decoration", "underline" );
+      //   // var el = $(e.currentTarget);
+
+      // };
+
+      // var tds = $( "td");
+      //    console.log(tds);
+      
+
+    }]),
+
+  link: function (scope, iElement, iAttrs, ctrl) {
+    var panel = $(iElement),
+      iSearch = panel.find('.glyphicon-search'),
+      iResetFind = panel.find('.glyphicon-remove'),
+      cells = null,
+      inputFind = panel.find('.find-user'),
+      findUser = inputFind.val(),
+      listphone = $('.listphone'),
+      tilephone = $('.tilephone');
+
+    iSearch.on('click',  function(event) {
+      event.preventDefault();
+
+      if ( $('.listphone td') ) {
+        cells = $('.listphone td');
+        return;
+      } 
+
+      // TODO: complete find for tilephone
+
+      cells = $('.tilephone div'); 
+    });
+
+    inputFind.on('keyup', function(event) {
+      event.preventDefault();
+      var txt = inputFind.val();
+
+      cells.each(function(i) {
+        var spans = $(this).find('span:gt(0)');
+        spans.each(function(i) {
+
+          if ( $(this).text() == '' ) {
+            return;
+          }
+          
+          if ( $(this).text() == txt ) {
+            $(this).closest('tr').addClass('active');
+            return;
+          }
+
+        });
+
+      });
+
+    });
+
+    inputFind.on('keyup', function(event) {
+
+      if (inputFind.val() == '') {
+        $('.table').find('tr').each(function() {
+      
+          if ( $(this).hasClass('active') ) {
+            $(this).removeClass('active');
+          }
+
+        });
+      }
+    });
+
+    iResetFind.on('click', function(event) {
+      event.preventDefault();
+
+      if (listphone) {
+        $('.table').find('tr').each(function() {
+      
+          if ( $(this).hasClass('active') ) {
+            $(this).removeClass('active');
+          }
+
+        });
+
+      } else {
+        $('.thumbnail').each(function() {
+      
+          if ( $(this).hasClass('active') ) {
+            $(this).removeClass('active');
+          }
+
+        });
+      }
+      
+    
+
+      inputFind.val("");
+        
+
+    });
+
+  }
+    
+}});
